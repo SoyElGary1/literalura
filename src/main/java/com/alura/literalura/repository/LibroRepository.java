537368becfd47049d326b979adfc7289ede6cb36
Christopher Nunez <elgary1129@gmail.com>
2024-12-31 19:15:32 -0300

    funciones en el repositorio de libro agregadas

--- src/main/java/com/alura/literalura/repository/LibroRepository.java
+++ src/main/java/com/alura/literalura/repository/LibroRepository.java
@@ -2,9 +2,18 @@ package com.alura.literalura.repository;
 
 import com.alura.literalura.model.Libro;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.awt.print.Book;
+import java.util.List;
 
 @Repository
 public interface LibroRepository extends JpaRepository<Libro, Long>{
+
+    @Query("SELECT b.idiomas FROM Libro b GROUP BY b.idiomas")
+    List<String> groupByBooksLanguage();
+
+    List<Libro> findByIdiomas(String idioma);
 
 }
