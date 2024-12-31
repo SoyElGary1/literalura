779ef2aba6a9c841943d1beb18a4527871f0de4e
Christopher Nunez <elgary1129@gmail.com>
2024-12-31 19:16:41 -0300

    Funciones para el repositorio de autor creadas

--- src/main/java/com/alura/literalura/repository/AutorRepository.java
+++ src/main/java/com/alura/literalura/repository/AutorRepository.java
@@ -1,7 +1,17 @@
 package com.alura.literalura.repository;
 
+import com.alura.literalura.model.Autor;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
-public interface AutorRepository {
+public interface AutorRepository  extends JpaRepository<Autor, Long> {
+
+    Optional<Autor> findByNombre(String name);
+
+    List<Autor> findByAnioDefuncionBefore(Integer date);
+
 }
